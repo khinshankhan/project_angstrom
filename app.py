8f52b07534023c87c6358e0725e15a9a2a823261
Khinshan Khan <kkhan@stuy.edu>
2018-05-16 14:37:40 -0400

    Made basic, hard-coded login with Ish

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template, session, redirect, url_for, flash,request, jsonify
+from flask import Flask, render_template, session, redirect, url_for, flash,request
 import random
 import os
 import requests
-from ast import literal_eval
-import ast, json, urllib2
 
 app = Flask(__name__)
 
 @app.route('/')
-def root_route():
-    return "home.html"
+def root():
+    if(loggedin()):
+        return "home.html"
+    else:
+        return "fake.html"
+
+def loggedin():
+    if ('user' in session and 'pass' in session):
+        if (valid(session['user']) and valid(session['pass'])):
+            return True
+    return False
+
+#will check against database later
+def valid():
+    if (session['user'] == 'admin' and  session['pass'] == 'safepass'):
+        return True
+    return False
+
+@app.route('login', methods = ['GET'. 'POST'])
+def login():
+    session['user'] = request.form['user']
+    session['pass'] = request.form['pass']
+    return redirect (url_for ('root'))
 
 if __name__ == "__main__":
     app.debug = True
