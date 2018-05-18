986abfdb15cae3e318a6d13070ef94057d972b8c
Ryan Siu <rsiu@stuy.edu>
2018-05-18 16:10:01 -0400

    Clean up stats file; move code into functions

--- utils/stats.py
+++ utils/stats.py
@@ -3,7 +3,6 @@ import random
 '''
 Stats to implement:
 - Max
-- Average
 - Num of matches
 - OPR: offensive power rating (calculated from aggregated alliance scores)
 - Scouted OPR (calculated from scouted information)
@@ -18,26 +17,29 @@ Stats to implement:
 from game_config import GAME_AUTO_2018 as AUTO
 from game_config import GAME_TELE_2018 as TELE
 
-SAMPLE_SCOUT_DATA = []
-
 # generate sample data
-for i in range(1, 11):
-    tasks = {}
+def generate():
+    data = []
+    for i in range(1, 11):
+        tasks = {}
 
-    for task in AUTO:
-        task_max = AUTO[task]['max'] if AUTO[task]['max'] else 3
-        tasks[task] = random.randint(AUTO[task]['min'], task_max)
+        for task in AUTO:
+            task_max = AUTO[task]['max'] if AUTO[task]['max'] else 3
+            tasks[task] = random.randint(AUTO[task]['min'], task_max)
 
-    for task in TELE:
-        task_max = TELE[task]['max'] if TELE[task]['max'] else 20
-        tasks[task] = random.randint(TELE[task]['min'], task_max)
+        for task in TELE:
+            task_max = TELE[task]['max'] if TELE[task]['max'] else 20
+            tasks[task] = random.randint(TELE[task]['min'], task_max)
 
-    SAMPLE_SCOUT_DATA.append({
-        'team': 310,
-        'match': i,
-        'tasks': tasks
-    })
+        data.append({
+            'team': 310,
+            'match': i,
+            'tasks': tasks
+        })
 
+    return data
+
+SAMPLE_SCOUT_DATA = generate()
 print SAMPLE_SCOUT_DATA
 s = str(SAMPLE_SCOUT_DATA).replace("'",'"')
 
@@ -48,16 +50,21 @@ print json.dumps(json.loads(s), indent=2, sort_keys=True)
 START CALCULATIONS
 '''
 
-OPRs = []
+# OPR
+def opr(data):
+    oprs = []
+    for match in data:
+        opr = 0
+        for task in match['tasks']:
+            if 'auto' in task:
+                opr += match['tasks'][task] * AUTO[task]['points']
+            else:
+                opr += match['tasks'][task] * TELE[task]['points']
+        oprs.append(opr)
+    return oprs
 
-for match in SAMPLE_SCOUT_DATA:
-    opr = 0
-    for task in match['tasks']:
-        if 'auto' in task:
-            opr += match['tasks'][task] * AUTO[task]['points']
-        else:
-            opr += match['tasks'][task] * TELE[task]['points']
-    OPRs.append(opr)
+OPRs = opr(SAMPLE_SCOUT_DATA)
+print 'Sample OPRs for each match:', OPRs
+print 'Sample overall OPR:', sum(OPRs) / len(OPRs)
 
-print OPRs
-print sum(OPRs) / len(OPRs)
+#
