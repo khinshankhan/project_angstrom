584ec1f46b506d8f9fc5e05aaac5fa5148ed5723
Ryan Siu <rsiu@stuy.edu>
2018-05-30 14:19:57 -0400

    Reorganize js for search team

---
+++ angstrom/static/js/search_team.js
@@ -0,0 +1,20 @@
+var search_bar = document.getElementById("search_team");
+
+search_bar.addEventListener("input", function() {
+  $.ajax({
+    url: "/find_teams?team_num=" + search_bar.value,
+    success: function(result){
+      update_collection(result);
+
+    }
+
+  });
+
+});
+
+var collection = document.getElementById("collection");
+
+var update_collection = function(html) {
+  collection.innerHTML = html;
+
+}
