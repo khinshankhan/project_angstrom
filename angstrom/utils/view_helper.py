68440d5acafa07e013b8923163e667b5ef780a22
Khinshan Khan <kkhan@stuy.edu>
2018-06-12 22:08:46 -0400

    tried to use semaphoric logic for matches and alliances

--- angstrom/utils/view_helper.py
+++ angstrom/utils/view_helper.py
@@ -159,8 +159,11 @@ def add_sample():
             "notes": ""
         })
         country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"]
+        semaphore = 0
+        doublesemaphore = 0
         entry = 7
-        for i in range(17,50):
+        match = 4
+        for i in range(17,25):
             country = random.randint(1,len(country_list)-1)
             add_team({
                 "team": i,
@@ -172,32 +175,32 @@ def add_sample():
             add_tasks_customdb({
                 "entry_id": entry,
                 "team_num": i,
-                "match_num": (i/30),
-                "alliance": random.randint(0,2),
+                "match_num": match,
+                "alliance": semaphore,
                 "user_id": 0,
             })
             entry += 1
             add_tasks_customdb({
                 "entry_id": entry,
                 "team_num": i,
-                "match_num": (i/30) + 1,
-                "alliance": random.randint(0,2),
+                "match_num": match + 1,
+                "alliance": semaphore,
                 "user_id": 0,
             })
             entry += 1
             add_tasks_customdb({
                 "entry_id": entry,
                 "team_num": i,
-                "match_num": (i/30) + 2,
-                "alliance": random.randint(0,2),
+                "match_num": match + 2,
+                "alliance": semaphore,
                 "user_id": 0,
             })
             entry += 1
             add_tasks_customdb({
                 "entry_id": entry,
                 "team_num": i,
-                "match_num": (i/30) + 3,
-                "alliance": random.randint(0,2),
+                "match_num": match + 3,
+                "alliance": semaphore,
                 "user_id": 0,
             })
             entry += 1
@@ -208,6 +211,16 @@ def add_sample():
                 "endgame": 0,
                 "notes": ""
             })
+            doublesemaphore += 1
+            if semaphore == 1 and doublesemaphore == 3:
+                match += 4
+            if doublesemaphore == 4:
+                doublesemaphore = 0
+            if semaphore == 1:
+                semaphore = 0
+            else:
+                semaphore = 1
+                
 
 if __name__ == "__main__":
     db_setup()
